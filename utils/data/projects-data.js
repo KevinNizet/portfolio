759cf65891015f0963f287d6d97f6d44ad0d3ae8
KevinNizet <kevin.nizet@gmail.com>
2024-04-13 10:47:37 +0200

    :bento: Update personnal projects

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,62 +1,90 @@
-import ayla from '/public/image/ayla.jpg';
-import crefin from '/public/image/crefin.jpg';
-import realEstate from '/public/image/real-estate.jpg';
-import travel from '/public/image/travel.jpg';
-
 export const projectsData = [
-    {
-        id: 1,
-        name: 'AI Powered Financial App',
-        description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
-        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
-        role: 'Backend Developer',
-        code: '',
-        demo: '',
-        image: crefin,
-    },
-    {
-        id: 2,
-        name: 'Travel Agency App',
-        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
-        role: 'Full Stack Developer',
-        code: '',
-        demo: '',
-        image: travel,
-    },
-    {
-        id: 3,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
-        code: '',
-        role: 'Full Stack Developer',
-        demo: '',
-        image: realEstate,
-    },
-    {
-        id: 4,
-        name: 'Newsroom Management',
-        description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
-        tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
-        code: '',
-        demo: '',
-        image: ayla,
-        role: 'Full Stack Developer',
-    }
+  {
+    id: 1,
+    type: "Site vitrine statique",
+    name: "Espace de coworking",
+    specifications:
+      "maquettage sur Figma, réflexion UX/UI, design responsive, sprint d'une semaine, hébergement du code sur GitHub",
+    description:
+      "réalisation d'un site vitrine pour un client souhaitant faire la promotion de son espace de coworking",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://github.com/KevinNizet/Coworking-Space",
+    code: "",
+    demo: "",
+    image: "",
+  },
+  {
+    id: 2,
+    type: "Application web dynamique",
+    name: "Let's Go !",
+    specifications:
+      "utilisation d’une API publique, possibilité de mise en favoris des événements, affichage conditionnel par filtres, design responsive, travail d'équipe en sprints d'une semaine, planification via Jira",
+    description:
+      "réalisation d'une application permettant aux utilisateurs de connaître les prochains événements culturels planifiés en région Pays de la Loire",
+    stack: ["React", "JavaScript", "CSS (SASS)", "HTML", "Figma", "Jira"],
+    link: "https://github.com/KevinNizet/LetsGo",
+    code: "",
+    demo: "",
+    image: "",
+  },
+  {
+    id: 3,
+    type: "Application web administrable",
+    name: "Inovin",
+    specifications:
+      "authentification utilisateur, profil administrateur, gestion des utilisateurs, mise en favoris, ajout de commentaires, algorithme proposant une sélection personnalisée, parcours utilisateur complet, formulaire de contact, téléchargement d’images",
+    description:
+      "réalisation d'une application administrable pour un professionnel souhaitant l'utiliser comme support pour des dégustations de vins personnalisées avec ses clients",
+    stack: [
+      "React",
+      "JavaScript",
+      "CSS (SASS)",
+      "Express",
+      "Node",
+      "MySQL",
+      "Figma",
+      "DB Designer",
+      "Jira",
+    ],
+    code: "",
+    link: "https://github.com/KevinNizet/Inovin",
+    demo: "",
+    image: "",
+  },
+  {
+    id: 4,
+    type: "Application web de petites annonces",
+    name: "The Good Corner",
+    specifications:
+      "signup utilisateur, authentification, création d'annonces, affichage conditionnel, pagination, dockerisation du projet",
+    description: "réalisation d'un clone du site Leboncoin",
+    stack: [
+      "React",
+      "JavaScript",
+      "TypeScript",
+      "NextJs",
+      "GraphQL",
+      "PostgreSQL",
+      "TypeORM",
+      "Apollo",
+      "Docker",
+    ],
+    code: "",
+    demo: "",
+    image: "",
+    link: "https://github.com/KevinNizet/the-good-corner",
+  },
+  {
+    id: 5,
+    type: "Mini jeu",
+    name: "Démineur Pokémon",
+    specifications: "CRUD complet, pixel design",
+    description:
+      "création d'un mini jeu Pokémon, sur le thème du démineur. Attrapez les pokémons en évitant la TeamRocket... Saurez-vous trouver où se cache le pokémon légendaire ?",
+    stack: ["React", "JavaScript", "MySQL", "Node", "Express"],
+    code: "",
+    demo: "",
+    image: "",
+    link: "https://github.com/KevinNizet/PokemonGame",
+  },
 ];
-
-
-// Do not remove any property.
-// Leave it blank instead as shown below
-
-// {
-//     id: 1,
-//     name: '',
-//     description: "",
-//     tools: [],
-//     role: '',
-//     code: '',
-//     demo: '',
-//     image: crefin,
-// },
